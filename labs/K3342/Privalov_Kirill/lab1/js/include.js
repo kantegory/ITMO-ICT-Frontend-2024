aeed20d94f6b042ddca09e73803e120a7abdab12
hellkirl <kirillprivalov04@icloud.com>
2024-10-24 22:23:04 +0400

    feature: add all files

---
+++ labs/K3342/Privalov_Kirill/lab1/js/include.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
