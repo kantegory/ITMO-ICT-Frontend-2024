d9e0e7e40d61dc15edc6d958904fbeccf6585de5
mariabakhareva <mary.in.world@mail.ru>
2024-11-27 22:39:27 +0300

    added the 3rd and 4th homeworks

--- labs/3342/lab2/frontend/js/include.js
+++ labs/3342/lab2/frontend/js/include.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Загрузка header.html
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
 
+            // Логика показа/скрытия пунктов меню
             const currentPage = window.location.pathname.split('/').pop();
             if (localStorage.getItem('token')) {
                 document.querySelector('a[href="login.html"]').parentElement.classList.add('d-none');
@@ -11,8 +13,31 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 document.querySelector('a[href="account.html"]').parentElement.classList.add('d-none');
             }
+
+            // Подключение обработчика переключения темы
+            const themeToggleButton = document.getElementById('theme-toggle');
+            if (!themeToggleButton) {
+                console.error('Кнопка переключения темы не найдена!');
+                return;
+            }
+
+            const body = document.body;
+
+            // Установка сохранённой темы
+            const savedTheme = localStorage.getItem('theme');
+            if (savedTheme === 'dark') {
+                body.classList.add('dark-theme');
+            }
+
+            // Обработчик переключения темы
+            themeToggleButton.addEventListener('click', () => {
+                body.classList.toggle('dark-theme');
+                const isDarkTheme = body.classList.contains('dark-theme');
+                localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+            });
         });
 
+    // Загрузка footer.html
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
